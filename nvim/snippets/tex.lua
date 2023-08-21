24693020ed0e5978aa2ab4ea4cf5fcc825fe974f
Adrian Bostic <66073333+gruelingpine185@users.noreply.github.com>
2023-08-20 22:11:46 -0400

    added several common environments

--- nvim/snippets/tex.lua
+++ nvim/snippets/tex.lua
@@ -19,28 +19,81 @@ return {
     s(
         {trig = 'lit', dscr = 'List item (tagged)'},
         fmta('\\item[<>] ', {i(1, 'tag')})
-    )
+    ),
+
+    -- Environments
     s(
-        {trig = 'ol', dscr = 'Ordered list'},
+        {trig = 'dc', dscr = 'Document class'},
+        fmta('\\documentclass{<>}', {i(1, 'class')})
+    ),
+    s(
+        {trig = 'bd', dscr = 'Begin Document environment'},
+        fmta(
+            [[
+                \begin{document}
+                    <>
+                \end{document}
+            ]],
+            {i(1)}
+        )
+    ),
+    s(
+        {trig = 'be', dscr = 'Begin environment'},
+        fmta(
+            [[
+                \begin{<>}
+                    <>
+                \end{<>}
+            ]],
+            {i(1, 'env'), i(2), rep(1)}
+        )
+    ),
+    s(
+        {trig = 'ol', dscr = 'Begin Ordered list'},
         fmta(
             [[
                 \begin{enumerate}
-                    <>
+                    \list <>
                 \end{enumerate}
             ]],
             {i(1)}
         )
     ),
     s(
-        {trig = 'ul', dscr = 'Unordered list'},
+        {trig = 'ul', dscr = 'Begin Unordered list'},
         fmta(
             [[
                 \begin{itemize}
-                    <>
+                    \list <>
                 \end{itemize}
             ]],
             {i(1)}
         )
     ),
-
+    s(
+        {trig = 'bdesc', dscr = 'Begin description environment'},
+        fmta(
+            [[
+                \begin{description}
+                    \list[<>] <>
+                \end{description}
+            ]],
+            {i(1, 'tag'), i(2)}
+        )
+    ),
+    s(
+        {trig = 'bim', dscr = 'Begin inlined math environment'},
+        fmta('$<>$', {i(1, 'math')})
+    ),
+    s(
+        {trig = 'bm', dscr = 'Begin display math environment'},
+        fmta(
+            [[
+                \[\[
+                    <>
+                \]\]
+            ]],
+            {i(1, 'math')}
+        )
+    )
 }
