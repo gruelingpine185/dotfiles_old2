dff1506154282645aeb392e5b726b3b69e1ffcae
gruelingpine185 <66073333+gruelingpine185@users.noreply.github.com>
2023-09-05 15:45:11 -0400

    added if statement and typedef macros for C

--- nvim/snippets/c.lua
+++ nvim/snippets/c.lua
@@ -12,6 +12,14 @@ local rep = require('luasnip.extras').rep
 
 return {
     -- Macros
+    -- s(
+    --     {trig = 'Inc', dscr = 'Global include'},
+    --     {
+    --         t('#include <'),
+    --         fmta('<>', i(1, 'header')),
+    --         t('>')
+    --     }
+    -- ),
     s(
         {trig = 'inc', dscr = 'Local include'},
         fmta('#include \"<>\"', {i(1, 'header')})
@@ -46,4 +54,59 @@ return {
             {i(1)}
         )
     ),
+    s(
+        {trig = 'td', dscr = 'Create typedef'},
+        fmta('typedef <> <>;', {i(1, 'type'), i(2, 'name')})
+    ),
+    s(
+        {trig = 'ts', dscr = 'Create typedef struct'},
+        fmta('typedef struct <> <>;', {i(1, 'type'), rep(1)})
+    ),
+    s(
+        {trig = 'an', dscr = 'Create NULL check assertion'},
+        fmta('assert(<> != NULL);', {i(1, 'ptr')})
+    ),
+    s(
+        {trig = 'if', dscr = 'Create if-statement'},
+        fmta(
+            [[
+                if(<>) {
+                    <>
+                }
+            ]],
+            {i(1, 'expr'), i(2)}
+        )
+    ),
+    s(
+        {trig = 'ifn', dscr = 'Create if-not statement'},
+        fmta(
+            [[
+                if(!<>) {
+                    <>
+                }
+            ]],
+            {i(1, 'expr'), i(2)}
+        )
+    ),
+    s(
+        {trig = 'iif', dscr = 'Create inlined if-statement'},
+        fmta('if(<>) <>;', {i(1, 'expr'), i(2)})
+    ),
+    s(
+        {trig = 'iifn', dscr = 'Create inlined if-not statement'},
+        fmta('if(!<>) <>;', {i(1, 'expr'), i(2)})
+    ),
+    s(
+        {trig = 'new', dscr = 'Malloc check'},
+        fmta(
+            [[
+                <> <> = (<>) malloc(sizeof(<>));
+                if(!<>) return <>;
+            ]],
+            {
+                i(1, 'type'), i(2, 'name'), rep(1),
+                i(3, 'type'), rep(2), i(4, 'NULL')
+            }
+        )
+    )
 }
