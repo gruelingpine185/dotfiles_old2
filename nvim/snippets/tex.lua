b90b97f23e644527455a7e670cb1527da9d473bd
gruelingpine185 <66073333+gruelingpine185@users.noreply.github.com>
2023-12-09 02:52:20 -0500

    added environments

--- nvim/snippets/tex.lua
+++ nvim/snippets/tex.lua
@@ -56,6 +56,18 @@ return {
 
     -- text mode
     s(
+        {trig = 'bdef', dscr = 'Begin definition environment'},
+        fmta(
+            [[
+                \begin{definition}[<>
+                    <>
+                \end{definition}
+                <>
+            ]],
+            {i(1, 'title'), i(2), i(3)}
+        )
+    ),
+    s(
         {trig = 'beg', dscr = 'Begin example environment'},
         fmta(
             [[
@@ -64,7 +76,115 @@ return {
                 \end{example}
                 <>
             ]],
-            {i(1), i(2), i(3)}
+            {i(1, 'title'), i(2), i(3)}
+        )
+    ),
+    s(
+        {trig = 'prf', dscr = 'Create proof'},
+        fmta(
+            [[
+                \begin{altproof}[<>]
+                    <>
+                \end{altproof}
+                <>
+            ]],
+            {i(1, 'title'), i(2), i(3)}
+        )
+    ),
+    s(
+        {trig = 'lmm', dscr = 'Create lemma'},
+        fmta(
+            [[
+                \begin{lemma}[<>]
+                    <>
+                \end{lemma}
+                <>
+            ]],
+            {i(1, 'title'), i(2), i(3)}
+        )
+    ),
+    s(
+        {trig = 'prob', dscr = 'Create problem'},
+        fmta(
+            [[
+                \begin{problem}[<>]
+                    <>
+                \end{problem}
+                <>
+            ]],
+            {i(1, 'title'), i(2), i(3)}
+        )
+    ),
+
+    s(
+        {trig = 'sec', dscr = 'Create section'},
+        fmta(
+            [[
+                \section{<>}
+                    <>
+                <>
+            ]],
+            {i(1, 'title'), i(2), i(3)}
+        )
+    ),
+    s(
+        {trig = 'thm', dscr = 'Create theorem'},
+        fmta(
+            [[
+                \begin{theorem}[<>]
+                    <>
+                \end{theorem}
+                <>
+            ]],
+            {i(1, 'title'), i(2), i(3)}
+        )
+    ),
+    s(
+        {trig = 'ba', dscr = 'Begin align environment'},
+        fmta(
+            [[
+                \begin{align}
+                    <>
+                \end{align}
+                <>
+            ]],
+            {i(1), i(2)}
+        )
+    ),
+    s(
+        {trig = 'bm', dscr = 'Begin display math environment'},
+        fmta(
+            [[
+                \[
+                    <>
+                \]
+                <>
+            ]],
+            {i(1), i(2)}
+        )
+    ),
+    s(
+        {trig = 'ol', dscr = 'Begin Ordered list'},
+        fmta(
+            [[
+                \begin{enumerate}
+                    \item <>
+                \end{enumerate}
+                <>
+            ]],
+            {i(1), i(2)}
+        )
+    ),
+    s(
+        {trig = 'ul', dscr = 'Begin Unordered list'},
+        fmta(
+            [[
+                \begin{itemize}
+                    \item <>
+                \end{itemize}
+                <>
+            ]],
+            {i(1), i(2)}
         )
     ),
 
