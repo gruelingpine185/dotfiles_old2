361ccf5d7df65689f817f4cdf6c3f4359fbdfaa1
gruelingpine185 <66073333+gruelingpine185@users.noreply.github.com>
2024-01-12 13:04:55 -0500

    set nordic theme nvim

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,7 +1,9 @@
 require('core.options')
+
 require('core.plugins')
 require('plugins.lspconfig')
 
 
-vim.cmd('colorscheme palenightfall')
+-- vim.cmd('colorscheme palenightfall')
+vim.cmd('colorscheme nordic')
 vim.cmd('filetype plugin indent on')
